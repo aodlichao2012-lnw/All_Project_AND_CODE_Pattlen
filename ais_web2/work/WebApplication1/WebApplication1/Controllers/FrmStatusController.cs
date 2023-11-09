36d86c39f57c96cad40a62e8bf189f234ba240f5
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2023-11-09 10:08:11 +0700

    50

--- ais_web2/work/WebApplication1/WebApplication1/Controllers/FrmStatusController.cs
+++ ais_web2/work/WebApplication1/WebApplication1/Controllers/FrmStatusController.cs
@@ -63,24 +63,25 @@ namespace ais_web3.Controllers
             try
             {
                 session_ID = id;
-
-                Agenids = HttpContext.Request.Cookies["Agen" + session_ID].Value;
-                string SQL = "";
+                if(HttpContext.Request.Cookies["Agen" + session_ID] != null)
+                {
+                    Agenids = HttpContext.Request.Cookies["Agen" + session_ID].Value;
+                    string SQL = "";
                     SQL = "select CNFG_STATUS_CODE.DESCRIPTION  as DESCRIPTION  from CNFG_AGENT_INFO,CNFG_STATUS_CODE  where AGENT_ID = :AGENT_ID AND CNFG_AGENT_INFO.LOGON_EXT= CNFG_STATUS_CODE.STATUS_ID AND ROWNUM = 1";
                     // Conn.Open(SQL, Conn)
                     DataTable dt2 = null;
-                module = new Module2(session_ID);
-                module.Comman_Static2(SQL, new string[] { Agenids }, new string[] { ":AGENT_ID" }, ref dt2);
+                    module = new Module2(session_ID);
+                    module.Comman_Static2(SQL, new string[] { Agenids }, new string[] { ":AGENT_ID" }, ref dt2);
                     if (dt2 == null)
                     {
                         return "Unknow";
                     }
                     if (dt2.Rows.Count > 0)
                     {
-                        if(HttpContext.Request.Cookies["Tel" + session_ID] != null && HttpContext.Request.Cookies["Tel" + session_ID].Expires.Year != 2000)
+                        if (HttpContext.Request.Cookies["Tel" + session_ID] != null && HttpContext.Request.Cookies["Tel" + session_ID].Expires.Year != 2000)
                         {
-                     
-                        return "Busy";
+
+                            return "Busy";
                         }
                         else
                         {
@@ -88,6 +89,13 @@ namespace ais_web3.Controllers
                         }
                     }
                     return "Unknow";
+                }
+                else
+                {
+                    return "Not Login";
+                }
+            
+              
             
             }
             catch (Exception ex)
